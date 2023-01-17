effea8cc075f882c271f25181d352d42ff99a208
draekko <draekko.software@gmail.com>
2023-01-17 13:01:36 -0500

    update tool bash script

--- tool_output/build_apk.sh
+++ tool_output/build_apk.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-java -jar AndResGuard-cli-1.2.22.jar input.apk -config config.xml -out outapk -signatureType v2 -signature release.keystore testres testres testres
+## $1 = APK filename
+## $2 = keystore filename
+
+if [ "$1" == ""]; then
+    echo "ERROR: make sure to specify APK and keystore for signing"
+    exit 1
+fi
+if [ "$2" == ""]; then
+    echo "ERROR: make sure to specify APK and keystore for signing"
+    exit 1
+fi
+
+java -jar AndResGuard-cli-1.2.22.jar $1 -config config.xml -out outapk -signatureType v2 -signature $2 testres testres testres
+
+exit 0
