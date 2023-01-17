f605cfef46ce42143fb9a1b32eadc7aad9801d4f
draekko <draekko.software@gmail.com>
2023-01-17 13:05:42 -0500

    update build script for output apk on cmd line

--- tool_output/build_apk.sh
+++ tool_output/build_apk.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
-## $1 = APK filename
-## $2 = keystore filename
+## $1 = Input APK filename
+## $2 = Output APK filename
+## $3 = keystore filename
 
 if [ "$1" == ""]; then
-    echo "ERROR: make sure to specify APK and keystore for signing"
+    echo "ERROR: make sure to specify input and output APK and keystore for signing"
     exit 1
 fi
 if [ "$2" == ""]; then
-    echo "ERROR: make sure to specify APK and keystore for signing"
+    echo "ERROR: make sure to specify input and output APK and keystore for signing"
+    exit 1
+fi
+if [ "$3" == ""]; then
+    echo "ERROR: make sure to specify input and output APK and keystore for signing"
     exit 1
 fi
 
-java -jar AndResGuard-cli-1.2.22.jar $1 -config config.xml -out outapk -signatureType v2 -signature $2 testres testres testres
+java -jar AndResGuard-cli-1.2.22.jar $1 -config config.xml -out $2 -signatureType v2 -signature $3 testres testres testres
 
 exit 0
