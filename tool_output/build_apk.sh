cf8eb1ffebd06d297f33dd2d7297528749835388
draekko <draekko.software@gmail.com>
2023-01-21 10:00:30 -0500

    Update build_apk.sh

--- tool_output/build_apk.sh
+++ tool_output/build_apk.sh
@@ -5,18 +5,21 @@
 ## $3 = keystore filename
 
 if [ "$1" == ""]; then
+    echo "PARAMS: APK-FILE OUTPUT-DIRECTORY KEYSTORE STORE-PW KEY-PW STORE-ALIAS SIGNATURE-TYPE"
     echo "ERROR: make sure to specify input APK and output directory and keystore for signing"
     exit 1
 fi
 if [ "$2" == ""]; then
+    echo "PARAMS: APK-FILE OUTPUT-DIRECTORY KEYSTORE STORE-PW KEY-PW STORE-ALIAS SIGNATURE-TYPE"
     echo "ERROR: make sure to specify input APK and output directory and keystore for signing"
     exit 1
 fi
 if [ "$3" == ""]; then
+    echo "PARAMS: APK-FILE OUTPUT-DIRECTORY KEYSTORE STORE-PW KEY-PW STORE-ALIAS SIGNATURE-TYPE"
     echo "ERROR: make sure to specify input APK and output directory and keystore for signing"
     exit 1
 fi
 
-java -jar AndResGuard-cli-1.2.22.jar $1 -config config.xml -out $2 -signatureType v2 -signature $3 testres testres testres
+java -jar AndResGuard-cli-1.2.22.jar $1 -config config.xml -out $2 -signatureType $7 -signature $3 $4 $5 $6
 
 exit 0
